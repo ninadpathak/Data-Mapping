316e73f45fe9e27277ca6ab384940a6bfc520015
Ninad Pathak <ninad@outlook.in>
2017-09-28 02:21:14 +0530

    Publishing all code

---
+++ MultipleGraphs.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot([1, 4, 10, 14], 'ro')
+
+plt.show()
--- PyplotBasic.py
+++ PyplotBasic.py
@@ -1,4 +1,31 @@
 import matplotlib.pyplot as plt
+import numpy as np
 plt.plot([1,2,5,4,1,6])
 plt.ylabel('Experiment data')
+plt.show()
+
+# red dots instead of a blue line (default)
+plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+
+# Plotting multiple graphs
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
 plt.show()
